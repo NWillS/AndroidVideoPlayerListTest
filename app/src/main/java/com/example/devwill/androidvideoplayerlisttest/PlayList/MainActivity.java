ff27d8722e7082be474303309ccbe45317509884
S N <seidi.nakamura@st-ventures.jp>
2018-05-09 13:20:29 +0900

    Fixed. リストを動的に作るように修正

--- app/src/main/java/com/example/devwill/androidvideoplayerlisttest/PlayList/MainActivity.java
+++ app/src/main/java/com/example/devwill/androidvideoplayerlisttest/PlayList/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.example.devwill.androidvideoplayerlisttest.R;
 import com.example.devwill.androidvideoplayerlisttest.VideoList;
@@ -33,25 +34,27 @@ public class MainActivity extends AppCompatActivity implements VideoListRecycleV
         rv.setLayoutManager(llm);
         rv.setAdapter(adapter);
 
+        List<Integer> idList = new ArrayList<>();
+        idList.add(R.raw.five_more_minutes_please);
+        idList.add(R.raw.he_came_to_work_without_his_laptop_but_he_will_not_let_anything_stand_between_him_and_productivity);
+        idList.add(R.raw.hugh_jackman_runs_into_a_former_student);
+        idList.add(R.raw.mans_best_friend);
+        idList.add(R.raw.master_of_comedy);
+        idList.add(R.raw.owner_dresses_up_as_dogs_favourite_toy);
+        idList.add(R.raw.she_did_it_im_the_good_boy_so_lets_play);
+        idList.add(R.raw.so_close);
+        idList.add(R.raw.this_is_the_first_time_i_laughed_at_a_bull_fighting);
+        idList.add(R.raw.trailer_footage_vs_actual_gameplay);
+        idList.add(R.raw.wait_for_it);
+        idList.add(R.raw.when_she_says_her_parents_arent_home);
+
         videoList = new ArrayList<>();
-        videoList.add(new RowData("Five more minutes please",R.raw.five_more_minutes_please));
-        videoList.add(new RowData("He came to work without his laptop but he will not let anything stand between him and productivity",
-                R.raw.he_came_to_work_without_his_laptop_but_he_will_not_let_anything_stand_between_him_and_productivity));
-        videoList.add(new RowData("Hugh jackman runs into a former student",R.raw.hugh_jackman_runs_into_a_former_student));
-        videoList.add(new RowData("Mans best friend",R.raw.mans_best_friend));
-        videoList.add(new RowData("Master of comedy",R.raw.master_of_comedy));
-        videoList.add(new RowData("Owner dresses up as dogs favourite toy",R.raw.owner_dresses_up_as_dogs_favourite_toy));
-        videoList.add(new RowData("She did it im the good boy so lets play",R.raw.she_did_it_im_the_good_boy_so_lets_play));
-        videoList.add(new RowData("So close",R.raw.so_close));
-        videoList.add(new RowData("This is the first time i laughed at a bull",R.raw.this_is_the_first_time_i_laughed_at_a_bull_fighting));
-        videoList.add(new RowData("Trailer footage vs actual gameplay",R.raw.trailer_footage_vs_actual_gameplay));
-        videoList.add(new RowData("Wait for it",R.raw.wait_for_it));
-        videoList.add(new RowData("When she says her parents arent home",R.raw.when_she_says_her_parents_arent_home));
+        for(int id : idList){
+            videoList.add(new RowData(formatTitle(id),id));
+        }
         VideoList.setList(videoList);
         adapter.setVideoList(videoList);
         adapter.notifyDataSetChanged();
-
-
     }
 
     @Override
@@ -59,5 +62,12 @@ public class MainActivity extends AppCompatActivity implements VideoListRecycleV
         Intent intent = new Intent(getApplication(), VideoPlayerActivity.class);
         intent.putExtra("position",position);
         startActivity(intent);
+    }
+
+    private String formatTitle(int id){
+        String tmp = getResources().getResourceEntryName(id);
+        tmp = tmp.replace("_"," ");
+        tmp = tmp.substring(0, 1).toUpperCase() + tmp.substring(1).toLowerCase();
+        return tmp;
     }
 }
